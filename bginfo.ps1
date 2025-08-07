01e3fb2f486630f88ac370e6a3db92bd1537f250
jaydienGH <129021990+jaydienGH@users.noreply.github.com>
2025-08-07 14:59:44 -0400

    Update bginfo.ps1

--- bginfo.ps1
+++ bginfo.ps1
@@ -47,7 +47,7 @@ $bgInfoFolderContent = "$bgInfoFolder\*"
 $bgInfoUrl = "https://download.sysinternals.com/files/BGInfo.zip"
 $bgInfoZip = "C:\BgInfo\BGInfo.zip"
 $bgInfoEula = "C:\BgInfo\Eula.txt"
-$logonBgiUrl = "https://tinyurl.com/jaydienbgi"
+$logonBgiUrl = "https://tinyurl.com/JNSBGI"
 $logonBgiZip = "$bgInfoFolder\LogonBgi.zip"
 $bgInfoRegPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"
 $bgInfoRegKey = "BgInfo"
@@ -185,3 +185,4 @@ Write-Host ($writeEmptyLine + "# Script completed" + $writeSeperatorSpaces + $cu
  
 
 ## ------------------------------------------------------------------------
+
